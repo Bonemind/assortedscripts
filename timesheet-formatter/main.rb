c8630adacb77b20acce9e891499c231a629ebf06
bonemind <subhime@gmail.com>
2016-03-09 06:49:12 -0600

    Implemented exports

--- timesheet-formatter/main.rb
+++ timesheet-formatter/main.rb
@@ -2,8 +2,11 @@ require 'nokogiri'
 require 'pry'
 require './nokogiri_to_hash'
 require 'pp'
+require 'timerizer'
 
 doc = File.open("hours.xml") { |f| Nokogiri::XML(f) }
+
+OUT_FOLDER = "./out"
 
 # Get arrays for required data
 projects = Hash.xml_node_to_hash(doc.xpath('//projects').first)[:project]
@@ -13,6 +16,60 @@ tags = Hash.xml_node_to_hash(doc.xpath('//tags').first)[:tag]
 tasktags = Hash.xml_node_to_hash(doc.xpath('//taskTags').first)[:taskTag]
 # 2015-09-07T12:26:00+02:00;2015-09-07T12:43:00+02:00;Airexpressbus;API;;Dataontwerp;
 #
+
+def as_csv(task_hash, filename)
+	# Write a csv of the data
+	header = 'starts_at;ends_at;relation;order;sub_order;description'
+	lines = []
+	lines << header
+	task_hash.each do |task|
+		line = []
+		line << task[:startDate]
+		line << task[:endDate]
+		line << task[:project][:employer]
+		line << task[:project][:name]
+		line << task[:tags].map{|t| t[:name]}.join(',')
+		line << task[:description]
+		lines << line.join(';')
+	end
+	filename = OUT_FOLDER + "/#{filename}.csv"
+	File.open(filename, 'w') do |file|
+		file.write(lines.join("\r\n"))
+	end
+	puts "Wrote #{filename}"
+end
+
+def export(tasks, filename)
+	as_csv(tasks, filename)
+end
+
+def export_bydate(tasks, start_date, end_date)
+	start_date_parsed = DateTime.parse(start_date).to_date
+	end_date_parsed = DateTime.parse(end_date).to_date
+
+	# Select only those dates
+	previous_week_tasks = tasks.select do |t|
+		next t[:startDate] >= start_date_parsed && t[:startDate] <= end_date_parsed
+	end
+
+	# Write the csv
+	export(previous_week_tasks, "#{start_date_parsed}-to-#{end_date_parsed}")
+end
+
+def export_all(tasks)
+	export(tasks, "all-#{DateTime.now}")
+end
+
+def last_week_export(tasks)
+	# Get the date of the previous sunday and the monday before that
+	last_sunday = DateTime.now - DateTime.now.wday
+	last_monday = 6.days.before(last_sunday)
+	last_sunday = last_sunday.to_date.to_s
+	last_monday = last_monday.to_date.to_s
+	export_bydate(tasks, last_monday, last_sunday)
+end
+
+
 
 # Build tasks with references
 tasks.each do |task|
@@ -36,10 +93,6 @@ tasks.each do |task|
 		tag = tags.find{|hash| tt[:tagId] == hash[:tagId]}
 		task[:tags] << tag unless tag.nil?
 	end
-end
-
-def as_csv(task_hash)
-	
 end
 
 # Split tasks if they have breaks
@@ -75,5 +128,6 @@ tasks.each do |task|
 	end
 end
 
-
+export_all(split_tasks)
+last_week_export(split_tasks)
 binding.pry
